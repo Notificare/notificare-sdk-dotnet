4f52f20117fe09f08734fe722f8db3aad2634ca1
Helder Pinhal <helder@notifica.re>
2025-01-29 11:33:18 +0000

    chore: add automation scripts

---
+++ scripts/download-xcframeworks.rb
@@ -0,0 +1,57 @@
+require 'fileutils'
+require 'open-uri'
+require 'optparse'
+require 'ostruct'
+require 'zip'
+require_relative 'lib/project'
+require_relative 'lib/terminal'
+
+options = OpenStruct.new
+OptionParser.new do |opt|
+  opt.on('-v', '--version VERSION', String, 'The version to download.') { |o| options.version = o }
+end.parse!
+
+def options.require_argument(key, message)
+  raise OptionParser::MissingArgument, message if self[key].nil?
+end
+
+options.require_argument('version', 'The version is required. Please provide it by using -v or --version.')
+
+
+puts "▸ Creating temporary directory".green
+FileUtils.rm_rf('.tmp')
+FileUtils.mkdir('.tmp')
+
+puts "▸ Downloading XCFrameworks".green
+open("https://cdn.notifica.re/libs/ios/#{options.version}/cocoapods.zip") do |file|
+  Zip::File.open_buffer(file.read) do |zip_file|
+    zip_file.each do |f|
+      fpath = File.join('.tmp', f.name)
+      zip_file.extract(f, fpath)
+    end
+  end
+end
+
+puts "▸ Updating binding XCFrameworks".green
+ios_projects = CSProject.all.select { |e| e.platform == :ios }
+ios_projects.each do |project|
+  puts "▸ Updating #{project.directory}".blue
+
+  destination = File.join(project.directory, 'libs')
+
+  FileUtils.rm_rf(destination)
+  FileUtils.mkdir(destination)
+
+  project.xcframeworks.each do |xcframework|
+    puts "▸ Copying #{xcframework}".blue
+
+    FileUtils.cp_r(
+      File.join('.tmp', 'Notificare', xcframework),
+      destination
+    )
+  end
+end
+
+FileUtils.rm_rf('.tmp')
+
+puts "▸ Done. 🚀".green
---
+++ scripts/lib/project.rb
@@ -0,0 +1,166 @@
+require 'fileutils'
+require_relative 'system'
+require_relative 'terminal'
+
+class CSProject
+  attr_reader :component, :platform, :directory
+
+  def initialize(component:, platform:, directory:)
+    @component = component
+    @platform = platform
+    @directory = directory
+  end
+
+  class << self
+    def all
+      [
+        CSProject.new(
+          component: :core,
+          platform: :android,
+          directory: 'Notificare.Android.Binding'
+        ),
+        CSProject.new(
+          component: :core,
+          platform: :ios,
+          directory: 'Notificare.iOS.Binding'
+        ),
+        CSProject.new(
+          component: :inbox,
+          platform: :android,
+          directory: 'Notificare.Inbox.Android.Binding'
+        ),
+        CSProject.new(
+          component: :inbox,
+          platform: :ios,
+          directory: 'Notificare.Inbox.iOS.Binding'
+        ),
+        CSProject.new(
+          component: :inbox,
+          platform: :ios,
+          directory: 'Notificare.NotificationServiceExtension.iOS.Binding'
+        ),
+        CSProject.new(
+          component: :push,
+          platform: :android,
+          directory: 'Notificare.Push.Android.Binding'
+        ),
+        CSProject.new(
+          component: :push,
+          platform: :ios,
+          directory: 'Notificare.Push.iOS.Binding'
+        ),
+        CSProject.new(
+          component: :push_ui,
+          platform: :android,
+          directory: 'Notificare.Push.UI.Android.Binding'
+        ),
+        CSProject.new(
+          component: :push_ui,
+          platform: :ios,
+          directory: 'Notificare.Push.UI.iOS.Binding'
+        ),
+      ]
+    end
+
+    def find(component, platform)
+      all.find { |e| e.component == component && e.platform == platform }
+    end
+  end
+
+  def clean
+    Dir.chdir(directory) do
+      execute_system_command('dotnet clean')
+    end
+  end
+
+  def build
+    Dir.chdir(directory) do
+      execute_system_command('dotnet build')
+    end
+  end
+
+  def generate_binding_sources
+    raise 'Cannot generate sharpie sources for non-iOS projects.' if platform != :ios
+
+    puts "▸ Running Objective-Sharpie".blue
+    sharpie_out = run_objective_sharpie
+
+    copy_generated_api_definitions(sharpie_out)
+    copy_generated_structs_and_enums(sharpie_out)
+  end
+
+  def xcframeworks
+    file_contents = File.read(File.join(directory, "#{directory}.csproj"))
+    file_contents.scan(/libs\\|\/(\w+.xcframework)/).flatten
+  end
+
+  protected def root_namespace
+    file_contents = File.read(File.join(directory, "#{directory}.csproj"))
+    file_contents.match(/<RootNamespace>(.+)<\/RootNamespace>/).captures[0]
+  end
+
+  private def binding_scheme
+    file_contents = File.read(File.join(directory, "#{directory}.csproj"))
+    file_contents.match(/<SchemeName>(.+)<\/SchemeName>/).captures[0]
+  end
+
+  private def run_objective_sharpie
+    command = <<~COMMAND
+      sharpie bind --output=sharpie-out \
+        --namespace=#{root_namespace} \
+        --sdk=#{latest_sharpie_sdk} \
+        --scope=Headers \
+        Headers/#{binding_scheme}-Swift.h
+    COMMAND
+
+    framework = File.join(directory, 'bin', 'Debug', 'net9.0-ios', "#{directory}.resources", "#{binding_scheme}iOS.xcframework", 'ios-arm64', "#{binding_scheme}.framework")
+    Dir.chdir(framework) do
+      puts "▸ Running Objective-Sharpie".blue
+      puts command
+      execute_system_command(command)
+    end
+
+    "#{framework}/sharpie-out"
+  end
+
+  private def latest_sharpie_sdk
+    _, output = capture_system_command_output('sharpie xcode -sdks', stream_output: false)
+    output.match(/(iphoneos\d+\.\d+)/).captures[0]
+  end
+
+  private def depends_on_core
+    core_project = CSProject.find(:core, platform)
+
+    contents = File.read(File.join(directory, "#{directory}.csproj"))
+    contents.include?(core_project.directory)
+  end
+
+  private def copy_generated_api_definitions(sharpie_out)
+    filename = File.join(sharpie_out, 'ApiDefinitions.cs')
+    return unless File.file?(filename)
+
+    contents = File.read(filename)
+
+    puts "▸ Removing scheme's default namespace".blue
+    contents = contents.gsub(/using #{binding_scheme};(\n)?/, '')
+
+    if depends_on_core
+      puts "▸ Including core's namespace in imports".blue
+
+      contents = <<~CONTENT
+      using #{CSProject.find(:core, platform).root_namespace};
+
+      #{contents}
+      CONTENT
+    end
+
+    File.write(File.join(directory, 'ApiDefinitions.cs'), contents)
+  end
+
+  private def copy_generated_structs_and_enums(sharpie_out)
+    filename = File.join(sharpie_out, 'StructsAndEnums.cs')
+    return unless File.file?(filename)
+
+    FileUtils.cp(filename, directory)
+  end
+end
